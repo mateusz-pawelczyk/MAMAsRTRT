3e7e3a8dbc7f73dc9a96bc1f3ced48ef0abc19dd
mateusz-pawelczyk <mateuszpawelczyk2003@gmail.com>
2023-12-11 12:10:18 +0100

    added random number generator

--- Common.hlsl
+++ Common.hlsl
@@ -19,3 +19,52 @@ struct SphereAttributes {
 	//float3 normal; // Surface normal at intersection
 	float2 uv;     // Texture coordinates
 };
+
+#define RANDOM_IA 16807
+#define RANDOM_IM 2147483647
+#define RANDOM_AM (1.0/RANDOM_IM)
+#define RANDOM_IQ 127773
+#define RANDOM_IR 2836
+#define RANDOM_MASK 123459876
+
+// Represents the generator state.
+struct NumberGenerator {
+	int seed; // Used to generate values.
+};
+
+// Generates the next number in the sequence.
+int Cycle(int seed) {
+	seed ^= RANDOM_MASK;
+	int k = seed / RANDOM_IQ;
+	seed = RANDOM_IA * (seed - k * RANDOM_IQ) - RANDOM_IR * k;
+
+	if (seed < 0)
+		seed += RANDOM_IM;
+
+	seed ^= RANDOM_MASK;
+	return seed;
+}
+
+// Cycles the generator based on the input count. Useful for generating a thread unique seed.
+int CycleCount(int seed, uint count) {
+	for (uint i = 0; i < count; ++i)
+		seed = Cycle(seed);
+	return seed;
+}
+
+// Sets the seed
+int SetSeed(uint value) {
+	int seed = int(value);
+	return Cycle(seed);
+}
+
+// Returns the current random float from the seed.
+float GetCurrentFloat(int seed) {
+	return RANDOM_AM * seed;
+}
+
+// Returns a random float within the input range.
+float GetRandomFloat(int seed, float low, float high) {
+	float v = GetCurrentFloat(seed);
+	return low * (1.0f - v) + high * v;
+}
--- RayGen.hlsl
+++ RayGen.hlsl
@@ -15,54 +15,7 @@ cbuffer CameraParams : register(b0)
   float4x4 projectionI;
 }
 
-#define RANDOM_IA 16807
-#define RANDOM_IM 2147483647
-#define RANDOM_AM (1.0/RANDOM_IM)
-#define RANDOM_IQ 127773
-#define RANDOM_IR 2836
-#define RANDOM_MASK 123459876
 
-// Represents the generator state.
-struct NumberGenerator {
-	int seed; // Used to generate values.
-};
-
-// Generates the next number in the sequence.
-int Cycle(int seed) {
-	seed ^= RANDOM_MASK;
-	int k = seed / RANDOM_IQ;
-	seed = RANDOM_IA * (seed - k * RANDOM_IQ) - RANDOM_IR * k;
-
-	if (seed < 0)
-		seed += RANDOM_IM;
-
-	seed ^= RANDOM_MASK;
-	return seed;
-}
-
-// Cycles the generator based on the input count. Useful for generating a thread unique seed.
-int CycleCount(int seed, uint count) {
-	for (uint i = 0; i < count; ++i)
-		seed = Cycle(seed);
-	return seed;
-}
-
-// Sets the seed
-int SetSeed(uint value) {
-	int seed = int(value);
-	return Cycle(seed);
-}
-
-// Returns the current random float from the seed.
-float GetCurrentFloat(int seed) {
-	return RANDOM_AM * seed;
-}
-
-// Returns a random float within the input range.
-float GetRandomFloat(int seed, float low, float high) {
-	float v = GetCurrentFloat(seed);
-	return low * (1.0f - v) + high * v;
-}
 
 [shader("raygeneration")] void RayGen() {
   // Initialize the ray payload
@@ -82,6 +35,7 @@ float GetRandomFloat(int seed, float low, float high) {
 	  // Inside some shader logic, when you need a random value:
 	  ng.seed = Cycle(ng.seed);
 	  float randomValue = GetCurrentFloat(ng.seed);
+
 	  // Now use randomValue for your purposes
 
 
